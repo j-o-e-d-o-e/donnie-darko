f46fb7a94664c92203ed53e7acc790c92eae9135
j-o-e-d-o-e <d_joe@gmx.net>
2021-01-23 00:26:56 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-finale = new Date("2021-04-04")
-const date = new Date();
+const finale = new Date(2021, 3, 4, 0,0, 0);
 const months = ["January", "February", "March", "April"];
+const date = new Date();
 
 function current() {
     return months[date.getMonth()] + " " + date.getDate();
 }
 
 function remaining() {
+    date.setHours(0, 0, 0);
     return Math.round(Math.abs((finale - date) / (24 * 60 * 60 * 1000)));
 }

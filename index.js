b25271a7e46939c04667b0b0aa5929ea419efe35
j-o-e-d-o-e <d_joe@gmx.net>
2021-01-24 00:48:16 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const finale = new Date(2021, 3, 4, 0,0, 0);
+const msg_f = "VmVzc2VsIHJldHVybmVkLgo="
 const months = ["January", "February", "March", "April"];
 const date = new Date();
 
@@ -9,4 +10,8 @@ function current() {
 function remaining() {
     date.setHours(0, 0, 0);
     return Math.round(Math.abs((finale - date) / (24 * 60 * 60 * 1000)));
+}
+
+function msg(){
+    return window.atob(msg_f);
 }
